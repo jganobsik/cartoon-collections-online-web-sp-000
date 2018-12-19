d0f6637dd167fae19a005d7fed0bfffcddacbede
Joe Ganobsik <jganobsik@gmail.com>
2018-12-19 20:35:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,13 @@ def summon_captain_planet(calls)
   calls.collect {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  
+def long_planeteer_calls (calls)
+  is_long = false
+  calls.each do |call|
+    if call.length > 4
+      is_long = true 
+    end
+    return is_long
 end
 
 def find_the_cheese# code an argument here
